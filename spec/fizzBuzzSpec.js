7341175b41ea9511ba6b098ad40c5ce5fe23d4f4
Jon Sanders <jonsanders101@aol.com>
2017-10-23 23:05:20 +0100

    Add tests

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,14 +1,36 @@
 describe("fizzBuzz", function() {
-  it("returns 'fizz' when passed a multiple of 3", function() {
-    expect(fizzBuzz(3)).toBe("fizz");
+
+  describe("multiples of 3", function() {
+    it("returns 'fizz' when passed 3", function() {
+      expect(fizzBuzz(3)).toBe("fizz");
+    })
+    it("returns 'fizz' when passed 66", function() {
+      expect(fizzBuzz(3)).toBe("fizz");
+    })
   })
-  it("returns 'buzz' when passed a multiple of 5", function() {
-    expect(fizzBuzz(5)).toBe("buzz");
+
+  describe("multiples of 5", function() {
+    it("returns 'buzz' when passed 5", function() {
+      expect(fizzBuzz(5)).toBe("buzz");
+    })
+    it("returns 'buzz' when passed 25", function() {
+      expect(fizzBuzz(5)).toBe("buzz");
+    })
   })
-  it("returns 'fizzbuzz' when passed a multiple of 3 and 5", function() {
-    expect(fizzBuzz(15)).toBe("fizzbuzz");
+
+  describe("multiples of 3 and 5", function() {
+    it("returns 'fizzbuzz' when passed 15", function() {
+      expect(fizzBuzz(15)).toBe("fizzbuzz");
+    })
+    it("returns 'fizzbuzz' when passed 30", function() {
+      expect(fizzBuzz(15)).toBe("fizzbuzz");
+    })
   })
-  it("returns number if not a multiple of 3 or 5", function() {
-    expect(fizzBuzz(0)).toBe(0);
+
+  describe("returns the same number passed if not a multiple of 3, 5 or both", function() {
+    it("returns number if not a multiple of 3 or 5", function() {
+      expect(fizzBuzz(0)).toBe(0);
+    })
   })
+
 });
